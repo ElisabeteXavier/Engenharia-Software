4074f8e1aed36b2cdf4c988f56893a7c41b6cad4
ElisabeteXavier <elisabetebx@gmail.com>
2023-12-17 21:48:00 -0300

    Primeiro teste de unidade e função de visualização

--- primeiro_projeto/test.py
+++ primeiro_projeto/test.py
@@ -1,23 +1,23 @@
 
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+# from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service
 
-import unittest
+# import unittest
 
-# class TesteFirefox(unittest.TestCase):
-# def setUp(self):
-# geckodriver_path = r'./home/elisabete/Software/geckodriver-v0.33.0-linux-aarch64/geckodriver'
-# service = Service(executable_path=geckodriver_path)
-# options = webdriver.FirefoxOptions()
-# driver = webdriver.Firefox(service=service,options=options)
+# # class TesteFirefox(unittest.TestCase):
+# # def setUp(self):
+# # geckodriver_path = r'./home/elisabete/Software/geckodriver-v0.33.0-linux-aarch64/geckodriver'
+# # service = Service(executable_path=geckodriver_path)
+# # options = webdriver.FirefoxOptions()
+# # driver = webdriver.Firefox(service=service,options=options)
 
-service = Service()
-options = webdriver.ChromeOptions()
-driver = webdriver.Chrome(service=service, options=options)
-# def test_page_title(self):
-# driver.get("https://google.com.br")
+# service = Service()
+# options = webdriver.ChromeOptions()
+# driver = webdriver.Chrome(service=service, options=options)
+# # def test_page_title(self):
+# # driver.get("https://google.com.br")
 
 
-driver.get("http://localhost:8000")
-assert "Congratulations" in driver.title
-print("ok")
+# driver.get("http://localhost:8000")
+# assert "Congratulations" in driver.title
+# print("ok")
